a16d438abdb1a0bb6d5dbbdefce0cb03bc0e07ca
Maanya D V <maanyadv06@gmail.com>
2026-07-29 23:01:37 +0530

    App.jsx: Importing compnent and rendering

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -1,8 +1,8 @@
+ import { HelloWorld } from "./components/HelloWorld";
  function App() {
  return(
      <div>
-      <h1>Hello World</h1>
-      <h3>Welcome to my first reaact app</h3>
+      <HelloWorld/>
      </div>
   )
 }
